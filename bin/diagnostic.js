ad0601c33e1608a150bf71dbd34ecae3eedcd87b
laurenfazah <laurenfazah@gmail.com>
2016-03-21 09:58:33 -0400

    cleared out answer from bin/diagnostic.js

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,15 +1,1 @@
 'use strict';
-//We'll learn about require later in the course
-const ask = require('../lib/ask.js');
-
-var answer = 'not empty';
-
-while (answer !== '') {
-  answer = ask("Guess my secret? ");
-  if (answer >=1 && answer <=10) {
-   console.log("You guessed it!");
-   break;
- }else{
-   console.log('Please try a number between 1 and 10')
- }
-}
